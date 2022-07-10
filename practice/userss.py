10f45ba037f16466823ef3e0fd9d1f7ec793508a
steewo <steewojeril19@gmail.com>
2022-07-10 11:36:05 +0530

    mod

--- practice/userss.py
+++ practice/userss.py
@@ -1,6 +1,7 @@
 
 # user with most followers
 # print followingg allathavarude suggestions (eg:id 2 userkk ulla suggestion)
+import random
 from json import *
 with open("users.json") as f:
     data=load(f)
@@ -10,8 +11,9 @@ print(fol)
 print(max(data,key=lambda user:len(user["followers"])))
 
 
-my_fol=[user["following"] for user in data if user["id"]==2]
+my_fol=[user["following"] for user in data if user["id"]==1]
 my_fol=my_fol.pop() # pop cheythal lastthe element kittum(last aayum firt aayum orenname ullu) [0] kk pakaram use cheyyunnath
 print(my_fol)
 user=[user for user in data if user["id"] not in my_fol]
-print(user)
+random.shuffle(user)
+print(user[:2])
