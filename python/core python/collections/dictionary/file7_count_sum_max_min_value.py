0ee01a9b22cfcb57ae953ef374a5d2594bdf0b9f
steewo jeril <steewojeril19@gmail.com>
2023-05-26 09:44:38 +0530

    added new file

---
+++ python/core python/collections/dictionary/file7_count_sum_max_min_value.py
@@ -0,0 +1,30 @@
+dic={"a":1,"b":2,"c":3}
+
+
+# to get keys only (will get as list)
+print(dic.keys())
+# to get values only (will get as list)
+print(dic.values())
+
+# WORD COUNT  (short method)
+# to print count of key provided from a dic  
+test_string="cat dog rabbit tiger elephant turtle"
+words_list=test_string.split(' ')                   #['cat', 'dog', 'rabbit', 'tiger', 'elephant', 'turtle']
+a={animals: words_list.count(animals) for animals in words_list}
+print(a)        #{'cat': 1, 'dog': 1, 'rabbit': 1, 'tiger': 1, 'elephant': 1, 'turtle': 1}
+
+
+
+# SUM:
+# sum of values of dic:
+print(sum(dic.values()))
+
+# MAX AND MIN
+# key having max value from dic:
+print(max(dic, key=dic.get))
+# key having min value from dic:
+print(min(dic,key=dic.get))
+    # max value from dic:
+print(max(dic.values()))
+    # min value from dic:
+print(min(dic.values()))
