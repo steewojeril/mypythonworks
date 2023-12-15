acdab8b3a48531437f701fb6e266435151eeadc0
steewo jeril <steewojeril19@gmail.com>
2023-12-15 13:14:49 +0530

    changes

--- python/adcance python/extra/7 max and min from dictionary.py
+++
@@ -1,9 +0,0 @@
-# to find max and min from dictionary
-dic1={'physics':56,'maths':65,'history':80}
-print(dic1)
-
-# print subject having lowest score
-
-# print(min(dic1)) #this doesn't work
-print(min(dic1,key=dic1.get))
-print(max(dic1,key=dic1.get))
--- python/core python/collections/dictionary/file7_count_sum_max_min_value.py
+++ python/core python/collections/dictionary/file7_count_sum_max_min_value.py
@@ -5,6 +5,8 @@ dic={"a":1,"b":2,"c":3}
 print(dic.keys())
 # to get values only (will get as list)
 print(dic.values())
+# to get dic as list of tuple  [('k1',v1),('k',v2),('k3',v3)]
+print(dic.items())
 
 # WORD COUNT  (short method)
 # to print count of key provided from a dic  
@@ -19,12 +21,26 @@ print(a)        #{'cat': 1, 'dog': 1, 'rabbit': 1, 'tiger': 1, 'elephant': 1, 't
 # sum of values of dic:
 print(sum(dic.values()))
 
-# MAX AND MIN
-# key having max value from dic:
-print(max(dic, key=dic.get))
-# key having min value from dic:
-print(min(dic,key=dic.get))
-    # max value from dic:
+# MAX 
+# return key having max key from dic:
+print(max(dic)) # or
+print(max(dic.keys()))
+
+# return max value from dic:
 print(max(dic.values()))
-    # min value from dic:
+
+# return key having max value from dic:
+print(max(dic, key=dic.get))
+
+# .................................................
+# Min
+# return key having min key from dic:
+print(min(dic)) # or
+print(min(dic.keys()))
+
+# return min value from dic:
 print(min(dic.values()))
+
+# return key having min value from dic:
+print(min(dic,key=dic.get))
+
